7e30c9feee245f19b71fedafcc3867f248d6e0ce
Ihor Abovian <abovyan1903@gmail.com>
2026-03-23 19:44:11 -0400

    feat: add swiper

--- js/main.js
+++ js/main.js
@@ -1,27 +1,10 @@
-// Ждем, пока весь HTML загрузится
-document.addEventListener('DOMContentLoaded', () => {
-  
-  const swiper = new Swiper('.mySwiper', {
-    // Настройки
-    slidesPerView: 1,      // 1 слайд на мобильном
-    spaceBetween: 20,     // Расстояние между карточками
-    centeredSlides: true, // Активный слайд по центру
-    loop: true,           // Бесконечная прокрутка
-    
-    // Точки внизу
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
 
-    // Адаптив под разные экраны
-    breakpoints: {
-      // Если экран шире 768px (планшеты и ПК)
-      768: {
-        slidesPerView: "auto", // Слайды будут такой ширины, как в CSS
-        spaceBetween: 30,
-      }
-    }
-  });
-
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
