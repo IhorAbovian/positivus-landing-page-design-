1b727f8d8c79ad84b46b5d74c8bfabe1f4f137b6
Ihor Abovian <abovyan1903@gmail.com>
2026-03-23 21:31:51 -0400

    feat: add swiper

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-const swiper = new Swiper(".swiper", {
-  slidesPerView: 1,
-  spaceBetween: 20,
+// Слайдер проектов (Case Studies)
+const projectsSwiper = new Swiper(".projects-slider", {
   loop: true,
-
   pagination: {
-    el: ".swiper-pagination",
+    el: ".projects-slider .swiper-pagination",
     clickable: true,
+  },
+});
+
+// Слайдер отзывов (Testimonials)
+const feedbackSwiper = new Swiper(".feedback-slider", {
+  loop: true,
+  pagination: {
+    el: ".feedback-slider .swiper-pagination",
+    clickable: true,
+    // Чтобы точки не имели лишних отступов и слушались твоих flex-контейнеров
+    bulletClass: "swiper-pagination-bullet !m-0", 
+  },
+  navigation: {
+    nextEl: ".feedback-slider .swiper-button-next-custom",
+    prevEl: ".feedback-slider .swiper-button-prev-custom",
   },
 });
